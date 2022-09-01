1fbb3f0145689984e8a4959e3ddcecd6764aa6c8
Mat Brunt <matbrunt@gmail.com>
2022-09-01 15:05:33 +0100

    Ch4. Delete files

--- fileSystem/walk/main_test.go
+++ fileSystem/walk/main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -51,6 +54,109 @@ func TestRun(t *testing.T) {
 
 			if tc.expected != res {
 				t.Errorf("expected %q, got %q instead\n", tc.expected, res)
+			}
+		})
+	}
+}
+
+// Test helper function is defined by calling t.Helper from the testing package
+// (this means if t.Fatal is called, Go prints the line in the test function that
+// called the helper function, rather the helper function line)
+func createTempDir(t *testing.T, files map[string]int) (dirname string, cleanup func()) {
+	t.Helper()
+
+	// create temporary directory with prefix 'walktest'
+	tempDir, err := os.MkdirTemp("", "walktest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// iterate over files map, creating specified number of dummy files for each extension
+	for k, n := range files {
+		for j := 1; j <= n; j++ {
+			fname := fmt.Sprintf("file%d%s", j, k)
+			fpath := filepath.Join(tempDir, fname)
+			if err := os.WriteFile(fpath, []byte("dummy"), 0644); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+
+	return tempDir, func() { os.RemoveAll(tempDir) }
+}
+
+func TestRunDelExtension(t *testing.T) {
+	testCases := []struct {
+		name        string
+		cfg         config
+		extNoDelete string
+		nDelete     int
+		nNoDelete   int
+		expected    string
+	}{
+		{
+			name:        "DeleteExtensionNoMatch",
+			cfg:         config{ext: ".log", del: true},
+			extNoDelete: ".gz", nDelete: 0, nNoDelete: 10,
+			expected: "",
+		},
+		{
+			name:        "DeleteExtensionMatch",
+			cfg:         config{ext: ".log", del: true},
+			extNoDelete: "", nDelete: 10, nNoDelete: 0,
+			expected: "",
+		},
+		{
+			name:        "DeleteExtensionMixed",
+			cfg:         config{ext: ".log", del: true},
+			extNoDelete: ".gz", nDelete: 5, nNoDelete: 5,
+			expected: "",
+		},
+	}
+
+	// Execute RunDel test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var (
+				buffer    bytes.Buffer
+				logBuffer bytes.Buffer
+			)
+
+			tempDir, cleanup := createTempDir(t, map[string]int{
+				tc.cfg.ext:     tc.nDelete,
+				tc.extNoDelete: tc.nNoDelete,
+			})
+			// Can also use t.Cleanup as a test helper function for cleaning up after tests have ran
+			defer cleanup()
+
+			// assign address of log bytes buffer to the config log instance
+			tc.cfg.wLog = &logBuffer
+
+			if err := run(tempDir, &buffer, tc.cfg); err != nil {
+				t.Fatal(err)
+			}
+
+			res := buffer.String()
+
+			if tc.expected != res {
+				t.Errorf("expected %q, got %q instead\n", tc.expected, res)
+			}
+
+			filesLeft, err := os.ReadDir(tempDir)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(filesLeft) != tc.nNoDelete {
+				t.Errorf("expected %d files left, got %d instead\n", tc.nNoDelete, len(filesLeft))
+			}
+
+			// compare number of log lines (+1 for trailing newline) in log output against number of deleted files
+			expLogLines := tc.nDelete + 1
+			// split bytes buffer on newline byte ([]byte("\n"))
+			lines := bytes.Split(logBuffer.Bytes(), []byte("\n"))
+			if len(lines) != expLogLines {
+				t.Errorf("expected %d log lines, got %d instead\n", expLogLines, len(lines))
 			}
 		})
 	}
